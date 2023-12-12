ce8c7c8f8111c278ec8c9887c68e0f5d75ad56f8
Nurbek Shukan <shukannurbek@gmail.com>
2023-12-12 18:29:30 +0100

    Implemented bst tree with heights to start off with AVL tree

--- AVL.hpp
+++ AVL.hpp
@@ -1,4 +1,113 @@
 #ifndef AVL_HPP
 #define AVL_HPP
 
+#include <iostream>
+#include <stack>
+
+struct Node {
+   Node* left;
+   Node* right;
+   int value;
+   int height;
+
+   Node(int _value): value(_value), left(nullptr), right(nullptr), height(0) {}
+};
+
+class AVLTree {
+private:
+   Node* root;
+
+   void printRecursive(Node* node, int indent = 0) const {
+      if (node != nullptr) {
+         printRecursive(node->right, indent + 5);
+         for (int i = 0; i < indent; ++i) {
+            std::cout << " ";
+         }
+         std::cout << "|-->" << node->value << "(" << node->height << ")" << std::endl;
+         printRecursive(node->left, indent + 4);
+      }
+   }
+
+   int subtree_height(Node* ptr) {
+      if (ptr == nullptr) {
+         return 0;
+      }
+      return ptr->height;
+   }
+
+   int balance(Node* ptr) {
+      return subtree_height(ptr->right) - subtree_height(ptr->left);
+   }
+
+   void delete_tree(Node* root) {
+      if (root != nullptr) {
+         delete_tree(root->left);
+         delete_tree(root->right);
+         delete root;
+      }
+   }
+public:
+   AVLTree(): root(nullptr) {}
+
+   ~AVLTree() {
+      delete_tree(root);
+   }
+
+   void insert(int val) {
+      std::stack<Node*> path;
+      Node* parent = nullptr;
+      Node* curr = root;
+      while(curr != nullptr) {
+         path.push(curr);
+         if (val < curr->value) {
+            parent = curr;
+            curr = curr->left;
+         } else {
+            parent = curr;
+            curr = curr->right;
+         }
+      }
+
+      Node* inserted = new Node(val);
+
+      if (parent == nullptr) {
+         root = inserted;
+         path.push(root);
+      } else {
+         if (val <= parent->value) {
+            parent->left = inserted;
+            path.push(parent->left);
+         } else {
+            parent->right = inserted;
+            path.push(parent->right);
+         }
+      }
+
+      while(!path.empty()) {
+         Node* tmp = path.top();
+         path.pop();
+         tmp->height = 1 + std::max(subtree_height(tmp->left), subtree_height(tmp->right));
+      }
+   }
+
+   bool find(int val) {
+      Node* curr = root;
+      while(curr != nullptr) {
+         if (val == curr->value) {
+            return true;
+         } else if (val < curr->value) {
+            curr = curr->left;
+         } else if (val > curr->value) {
+            curr = curr->right;
+         }
+      }
+
+      return false;
+   }
+
+   void print() {
+      printRecursive(root);
+   }
+};
+
 #endif
--- test.cpp
+++ test.cpp
@@ -3,22 +3,22 @@
 #include "BST.hpp"
 
 int main() {
-   Tree bst_tree;
+   AVLTree tree;
 
-   bst_tree.insert(10);
-   bst_tree.insert(5);
-   bst_tree.insert(3);
-   bst_tree.insert(7);
-   bst_tree.insert(15);
-   bst_tree.insert(11);
-   bst_tree.insert(20);
+   tree.insert(10);
+   tree.insert(5);
+   tree.insert(3);
+   tree.insert(7);
+   tree.insert(15);
+   tree.insert(11);
+   tree.insert(20);
    
-   if (bst_tree.find(3)) {
+   if (tree.find(3)) {
       std::cout << "3 is in the tree." << std::endl;
    } else {
       std::cout << "3 is not in the tree." << std::endl;
    }
    
-   bst_tree.print();
+   tree.print();
    
 }
