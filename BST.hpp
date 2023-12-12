c714a28b7184c92ec879bf2806c5b267831addd5
Nurbek Shukan <shukannurbek@gmail.com>
2023-12-12 18:07:21 +0100

    Implemented first version of BST tree

--- BST.hpp
+++ BST.hpp
@@ -1,4 +1,90 @@
 #ifndef BST_HPP
 #define BST_HPP
 
+#include <iostream>
+
+struct TreeNode {
+   TreeNode* left;
+   TreeNode* right;
+   int value;
+
+   TreeNode(int val): value(val), left(nullptr), right(nullptr) {}
+};
+
+class Tree {
+private:
+   TreeNode* root;
+
+   void printRecursive(TreeNode* node, int indent = 0) const {
+      if (node != nullptr) {
+         printRecursive(node->right, indent + 5);
+         for (int i = 0; i < indent; ++i) {
+            std::cout << " ";
+         }
+         std::cout << "|-->" << node->value << std::endl;
+         printRecursive(node->left, indent + 4);
+      }
+   }
+
+   void delete_tree(TreeNode* root) {
+      if (root != nullptr) {
+         delete_tree(root->left);
+         delete_tree(root->right);
+         delete root;
+      }
+   }
+public:
+   Tree(): root(nullptr) {}
+
+   ~Tree() {
+      delete_tree(root);
+   }
+
+   void insert(int val) {
+      TreeNode* parent = nullptr;
+      TreeNode* curr = root;
+      while(curr != nullptr) {
+         if (val < curr->value) {
+            parent = curr;
+            curr = curr->left;
+         } else {
+            parent = curr;
+            curr = curr->right;
+         }
+      }
+
+      TreeNode* inserted = new TreeNode(val);
+
+      if (parent == nullptr) {
+         root = inserted;
+      } else {
+         if (val <= parent->value) {
+            parent->left = inserted;
+         } else {
+            parent->right = inserted;
+         }
+      }
+   }
+
+   bool find(int val) {
+      TreeNode* curr = root;
+      while(curr != nullptr) {
+         if (val == curr->value) {
+            return true;
+         } else if (val < curr->value) {
+            curr = curr->left;
+         } else if (val > curr->value) {
+            curr = curr->right;
+         }
+      }
+
+      return false;
+   }
+
+   void print() {
+      printRecursive(root);
+   }
+};
+
+
 #endif
--- test.cpp
+++ test.cpp
@@ -3,5 +3,22 @@
 #include "BST.hpp"
 
 int main() {
+   Tree bst_tree;
+
+   bst_tree.insert(10);
+   bst_tree.insert(5);
+   bst_tree.insert(3);
+   bst_tree.insert(7);
+   bst_tree.insert(15);
+   bst_tree.insert(11);
+   bst_tree.insert(20);
+   
+   if (bst_tree.find(3)) {
+      std::cout << "3 is in the tree." << std::endl;
+   } else {
+      std::cout << "3 is not in the tree." << std::endl;
+   }
+   
+   bst_tree.print();
    
 }
